d7a164222e5db9b54435fc43318b8fd5bc9a217c
fajarkur219 <101600461+fajarkur219@users.noreply.github.com>
2022-03-16 10:32:22 +0700

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -1,7 +1,21 @@
- class Calculator
+class Calculator
  {
 	 static void main(string[] args)
 	 {
+		 int a = 10;
+		 int b = 6;
+		 console.Writeline("Hasil penambahan : {0} + {1} = {2}", a, b, penambahan(a, b));
+		 console.Writeline("Hasil pengurangan : {0} - {1} = {2}", a, b, pengurangan(a, b));
 		 
+		 console.Writeline("\Tekan Sembarang key untuk keluar");
+		 console.ReadKey();
 	 }
+	 static int Penambahan(int a, int b )
+	{
+		return a + b;
+	}
+	static int Pengurangan(int a, int b )
+	{
+		return a - b;
+	}
  }
